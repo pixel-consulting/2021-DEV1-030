b9a31f82a9c7c2dd357c4d5f4adb3a80584de06b
Anonymous <notset>
2021-01-01 19:05:45 +0100

    feat: implementation of transformer class (tranform GameEntity->Game), GameentityTransformer

--- src/main/java/com/digitalstork/tictactoe/util/GameEntityTransformer.java
+++ src/main/java/com/digitalstork/tictactoe/util/GameEntityTransformer.java
@@ -1,4 +1,20 @@
 package com.digitalstork.tictactoe.util;
 
-public class GameEntityTransformer {
+import com.digitalstork.tictactoe.dto.Game;
+import com.digitalstork.tictactoe.model.GameEntity;
+import com.digitalstork.tictactoe.model.MarkerEnum;
+
+import java.util.function.Function;
+
+public class GameEntityTransformer implements Function<GameEntity, Game> {
+
+    @Override
+    public Game apply(GameEntity game) {
+        String nextPlayer = game.getNextPlayer() == MarkerEnum.BLANK ? "X can start the game" : String.format("%s Player", game.getNextPlayer());
+        return Game.builder()
+                .id(game.getId())
+                .endGame(game.isEndGame())
+                .nextPlayer(nextPlayer)
+                .build();
+    }
 }
